f827ae539843513a6a3f01817314dcc98031bd47
ScriptFoundry <studio.scriptfoundry@gmail.com>
2025-11-21 22:43:20 +0100

    Fixed

--- src/ServerScriptService/NPCCore/Server.server.lua
+++ src/ServerScriptService/NPCCore/Server.server.lua
@@ -9,14 +9,9 @@ local npcData = {
 }
 
 -- Initialize the state machine (pass factories directly, not nested)
-local npcStateMachine = StateMachine.new(
+StateMachine.new(
 	"Idle",
 	StateMachine.LoadFromFolder(ReplicatedStorage.NPCSystem.Villager.States),
 	npcData,
-	{ UseHeartbeat = true, Debug = true, HeartbeatInterval = 0.2 } -- Enable debugging to see warnings
+	{ UseHeartbeat = true, Debug = true, HeartbeatInterval = 0 } -- Enable debugging to see warnings
 )
-
--- Listen for state changes
-npcStateMachine.StateChanged:Connect(function(newState, prevState)
-	print(newState, prevState)
-end)
