e750db210d2f58c1250bf45b1192a4c9dc699794
ScriptFoundry <studio.scriptfoundry@gmail.com>
2025-11-22 16:23:12 +0100

    Added Interacted, Idle, Wandering and other things

--- src/ServerScriptService/NPCCore/Server.server.lua
+++ src/ServerScriptService/NPCCore/Server.server.lua
@@ -3,15 +3,17 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local StateMachine = require(ReplicatedStorage.Packages.StateMachine)
 
 -- Create NPC-specific data
-local npcData = {
-	Health = 100,
-	NPC = workspace.NPCs.Villager,
-}
 
--- Initialize the state machine (pass factories directly, not nested)
-StateMachine.new(
-	"Idle",
-	StateMachine.LoadFromFolder(ReplicatedStorage.NPCSystem.Villager.States),
-	npcData,
-	{ UseHeartbeat = true, Debug = true, HeartbeatInterval = 0 } -- Enable debugging to see warnings
-)
+for _, NPC in workspace.NPCs:GetChildren() do
+	local npcData = {
+		Health = 100,
+		NPC = NPC,
+		Interacted = false,
+	}
+	StateMachine.new(
+		"Idle",
+		StateMachine.LoadFromFolder(ReplicatedStorage.NPCSystem.Villager.States),
+		npcData,
+		{ UseHeartbeat = true, Debug = true, HeartbeatInterval = 0 }
+	)
+end
